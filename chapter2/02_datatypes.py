3f4886cc006bb955bcbde959df12cc3bfd1f8038
AdityaBhamare-code <adityabhamare216@gmail.com>
2026-08-07 18:37:51 +0530

    Datatypes

---
+++ chapter2/02_datatypes.py
@@ -0,0 +1,11 @@
+# Datatypes 
+
+a = 1   # a is an integer
+
+b = 5.22   # b is an floating point number
+
+c = "aditya" # c is an string 
+
+d = False # d is a boolean variable
+
+e = None # e is a none type variable
