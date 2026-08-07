17e6cef01cc4f98c4d3add55aa74aed5e2f7399e
AdityaBhamare-code <adityabhamare216@gmail.com>
2026-08-07 18:53:40 +0530

    Rules in variables

---
+++ chapter2/03_rules_variables.py
@@ -0,0 +1,21 @@
+a = 23
+
+aaa = 435
+
+aadi = 34
+
+harsh = 45 
+
+_harsh = 57
+
+# @harsh = 78 // @ before name is not allowed in py.
+
+# 1. Variable name must start with a letter or an underscore (_)
+# 2. Variable name can contain letters, numbers, and underscores
+# 3. Spaces are not allowed in variable names
+
+# Operators in Python.
+# 1. Arithmetic Operators: +,-,*,/,%,**,//
+# 2. Comparison (Relational) Operators: ==,!=,>,<,>=,<= 
+# 3. Assignment operators: =,+=,-= 
+# 4. Logical Operators: and, or, not.
