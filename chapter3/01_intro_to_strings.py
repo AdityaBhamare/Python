72892d85502db5ebfb29ade52234eebafdfe9f61
AdityaBhamare-code <adityabhamare216@gmail.com>
2026-08-08 14:54:06 +0530

    Strings in py

---
+++ chapter3/01_intro_to_strings.py
@@ -0,0 +1,13 @@
+name = "Aditya"
+
+nameshort = name[0:3] # start from index 0 all the way till 3 (excluding 3)
+
+print(nameshort)
+
+# String is a data type in python.
+# String is a sequence of characters enclosed in quotes.
+# We can primarily write a string in these three ways.
+
+a = 'harry' # Single quoted string
+b = "harry" # Double quoted string
+c = '''harry''' # Triple quoted string
