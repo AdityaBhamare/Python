548054f2ffbb46f5a61a132aa4f061f8f8d00b8b
AdityaBhamare-code <adityabhamare216@gmail.com>
2026-08-09 15:01:05 +0530

    List Methods

---
+++ chapter4/02_list_methods.py
@@ -0,0 +1,11 @@
+friends = ["Apple", "Orange", 5, 345.06, False, "Aakash", "Rohan"]
+print(friends)
+friends.append("Aditya") # append insert element at the end.
+print(friends)
+
+l1 = [1, 34, 62, 2, 3, 11]
+# l1.sort()
+# l1.reverse()
+# l1.insert(3, 3333333) # Insert is used to insert the element at particular index we want.
+print(l1.pop(3)) # pop is used to delete the particular element at index and return its value. 
+print(l1)
