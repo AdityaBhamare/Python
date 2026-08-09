d7b121d7ab5ee1819fe85d78613db9c1a722b281
AdityaBhamare-code <adityabhamare216@gmail.com>
2026-08-09 18:44:16 +0530

    Dictnory in py

---
+++ chapter5/01_dict.py
@@ -0,0 +1,15 @@
+# Dictionary is a collection of keys-value pairs.
+# Properties:-
+# 1. It is unordered.
+# 2. It is mutable.
+# 3. It is indexed.
+# 4. Cannot contain duplicate keys.
+
+marks = {
+    "Aadi": 100,
+    "Harry": 80,
+    "Preet": 70
+}
+
+print(marks, type(marks))
+print(marks["Aadi"])
