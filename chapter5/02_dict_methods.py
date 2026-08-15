848aca79fa08d7a41d391ad065db7ab0a25146ba
AdityaBhamare-code <adityabhamare216@gmail.com>
2026-08-15 14:55:18 +0530

    dictnory methods

--- chapter5/02_dict_methods.py
+++ chapter5/02_dict_methods.py
@@ -1,7 +1,15 @@
 marks = {
     "Aadi": 100,
     "Harry": 80,
-    "Preet": 70
+    "Preet": 70,
+    0: "Aadi"
 }
 
-print(marks.items())
+# print(marks.keys())
+# print(marks.items())
+# print(marks.values())
+# marks.update({"Harry": 99})
+# print (marks)
+
+print(marks.get("Aadi2"))
+print(marks["Aadi2"])
