77c78c019b700531e298240c3d52011a590b438c
AdityaBhamare-code <adityabhamare216@gmail.com>
2026-08-22 14:48:57 +0530

    function def & fumction call

---
+++ chapter8/01_functions.py
@@ -0,0 +1,26 @@
+# a = int(input("Enter your number:"))
+# b = int(input("Enter your number:"))
+# c = int(input("Enter your number:"))
+
+# average = (a+b+c)/3
+# print(average)
+
+# Function defination
+def avg():
+    a = int(input("Enter your number:"))
+    b = int(input("Enter your number:"))
+    c = int(input("Enter your number:"))
+
+    average = (a+b+c)/3
+    print(average)
+
+# Function call
+avg()
+print("Thankyou!")
+avg()
+print("Thankyou!")
+avg()
+print("Thankyou!")
+avg()
+avg()
+
